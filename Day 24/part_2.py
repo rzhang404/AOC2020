fba8bae387121ce8a291eb4127ea9b800ce127a5
Ray Zhang <raybot@students.cs.ubc.ca>
2020-12-23 22:16:20 -0800

    Day 24 part 2 updated using sets.

--- Day 24/part_2.py
+++ Day 24/part_2.py
@@ -2,7 +2,7 @@ fo = open("input.txt","r")
 raw = fo.read()
 strs = raw.split()
 
-grid = {}
+grid = set()
 
 for tile in strs:
     mod = ""
@@ -34,20 +34,17 @@ for tile in strs:
 
     loc = str(x) + " " + str(y)
 
-    truetile = grid.get(loc, +1)
-    grid[loc] = truetile * -1
-
-aux_grid = grid.copy()
-for loc, truetile in aux_grid.items():
-    if truetile == +1:
-        grid.pop(loc, None)
+    if loc in grid:
+        grid.remove(loc)
+    else:
+        grid.add(loc)
 
 # assume grid only tracks black tiles
 # aux_grid = {}  # aux_grid holds next iteration of tiles
 for i in range(100):
     aux_grid = grid.copy()  # save current grid
     neighbour_grid = {}   # neighbour_grid counts neighbouring black tiles
-    for loc, truetile in grid.items():
+    for loc in grid:
         split = loc.split(" ")
         x, y = int(split[0]), int(split[1])
 
@@ -58,44 +55,17 @@ for i in range(100):
         neighbour_grid[str(x) + " " + str(y - 1)] = neighbour_grid.get((str(x) + " " + str(y - 1)), 0) + 1
         neighbour_grid[str(x + 1) + " " + str(y - 1)] = neighbour_grid.get((str(x + 1) + " " + str(y - 1)), 0) + 1
 
-
-
-        # neighbours = 0
-
-        # neighbours += 1 if grid.get(str(x+1) + " " + str(y), +1) == -1 else 0
-        # neighbours += 1 if grid.get(str(x) + " " + str(y+1), +1) == -1 else 0
-        # neighbours += 1 if grid.get(str(x-1) + " " + str(y+1), +1) == -1 else 0
-        # neighbours += 1 if grid.get(str(x-1) + " " + str(y), +1) == -1 else 0
-        # neighbours += 1 if grid.get(str(x) + " " + str(y-1), +1) == -1 else 0
-        # neighbours += 1 if grid.get(str(x+1) + " " + str(y-1), +1) == -1 else 0
-        #
-        # if truetile == +1:  # White flip condition
-        #     if neighbours == 2:
-        #         aux_grid[loc] = -1
-        #     else:
-        #         aux_grid[loc] = +1
-        # else:  # Black flip condition
-        #     if neighbours == 0 or neighbours > 2:
-        #         aux_grid[loc] = +1
-        #     else:
-        #         aux_grid[loc] = -1
-
     for loc, neighbour_count in neighbour_grid.items():
         if neighbour_count == 2:
-            aux_grid[loc] = -1  # regardless of starting colour, this tile is now black, so track it
+            aux_grid.add(loc)  # regardless of starting colour, this tile is now black, so track it
 
-    for loc, truetile in grid.items():
+    for loc in grid:
         neighbour_count = neighbour_grid.get(loc, 0)
         if neighbour_count == 0 or neighbour_count > 2:
-            aux_grid.pop(loc, None)  # if this tile was originally black, check if it still should be
+            aux_grid.discard(loc)  # if this tile was originally black, check if it still should be
 
     grid = aux_grid
 
 
-total = 0
-for truetile in grid.values():
-    if truetile == -1:
-        total += 1
-
-print(total)
+print(len(grid))
 
